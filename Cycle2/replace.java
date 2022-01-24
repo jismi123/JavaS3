3e307239420bf864a6fbad7e4e1321af9f87a46c
jismi123 <73015077+jismi123@users.noreply.github.com>
2022-01-24 18:55:09 +0530

    Create replace.java
    To replace a particular element in an array

---
+++ Cycle2/replace.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class replace_element 
+{
+    public static void main(String args[])
+    {
+        int i;
+        int a[]=new int[20];
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n=obj.nextInt();
+        System.out.println("Enter the elements:");
+        for(i=0;i<n;i++)
+        {
+            a[i]=obj.nextInt();
+        }
+        System.out.println("Enter the element to be replaced:");
+        int r=obj.nextInt();
+        System.out.println("Enter new element:");
+        int element=obj.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==r)
+            {
+                a[i]=element;
+            }
+        }
+        System.out.println("New list of elements:");
+        for(i=0;i<n;i++)
+        {
+            System.out.println(""+a[i]);
+        }
+
+    }
+}
