3a1c4e2f36ee761c9a3383c479c477a06a6da5b6
jismi123 <73015077+jismi123@users.noreply.github.com>
2022-01-26 12:44:20 +0530

    Update perimeter_shapes.java

--- Cycle3/perimeter_shapes.java
+++ Cycle3/perimeter_shapes.java
@@ -1,15 +1,15 @@
 import java.util.*;
 class perimeter_shapes
 {
-	public static void main(String args[])
-	{ 
+    public static void main(String args[])
+    { 
        Scanner obj = new Scanner(System.in);
        System.out.print("Enter radius of circle :");
-       int r = obj.nextInt();
+       double r = obj.nextDouble();
        
        System.out.print("Enter length and breadth of rectangle :");
-       int l = obj.nextInt();
-       int b = obj.nextInt();
+       double l = obj.nextDouble();
+       double b = obj.nextDouble();
        
        perimeter obj1 = new perimeter(r);
        obj1 = new perimeter(l,b);
@@ -18,16 +18,16 @@ class perimeter_shapes
 
 class perimeter
 {
-    perimeter(int radius)
+    perimeter(double radius)
     {
         double Perimeter = 3.14*2*radius;
         System.out.println("Perimeter of circle = "+Perimeter);
     }
     
-    perimeter(int len,int breadth)
+    perimeter(double len,double breadth)
     {
         double Perimeter = 2*(len+breadth);
         System.out.println("Perimeter of rectangle = "+Perimeter);
     }
-    
 }
+    
